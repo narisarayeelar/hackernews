a7e011ef601cece5a01c760add9117f997b8475d
narisara <y.narisara@gmail.com>
2019-11-19 23:42:37 +0700

    Update chapter2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 const list = [
@@ -21,10 +20,15 @@ const list = [
   }
 ];
 
+const isSearch = (searchTerm) => (item) => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { list };
+    this.state = {
+      list,
+      searchTerm: ''
+    };
     //New dont'have to do bind (this) to class method. It automatically bind
     // this.onDismiss = this.onDismiss.bind(this);
   }
@@ -46,30 +50,128 @@ class App extends Component {
   //********************************************************************** */
   //Class methods can be auto-bound using JavaScript ES6 arrow functions:
   //********************************************************************** */
-  onDismiss = (id) =>{
-    
-    const isNotId = (item) => item.objectID !==id;
+  onDismiss = (id) => {
+    const isNotId = (item) => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
-    this.setState({list:updatedList});
+    this.setState({ list: updatedList });
   }
 
+
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  //// ES5
+  // isSearch(searchTerm){
+  //   return function(item){
+  //     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  //   }
+  // }
+
   render() {
+
+    const { list, searchTerm } = this.state;
+
     return (
-      <div className="App">
-        {this.state.list.map(item => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <button onClick={() => this.onDismiss(item.objectID)} type="button">
-              Dismiss
-            </button>
-          </div>
-        ))}
+      <div className="page">
+        <div className="interactions">
+          <Search
+            searchTerm={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
+    );
+  }
+}
+
+// class Search extends Component {
+//   render() {
+//     const { searchTerm, onSearchChange, children } = this.props
+//     return (
+//       <form>
+//         {children}
+//         <input type="text" onChange={onSearchChange} value={searchTerm} />
+//       </form>
+//     )
+//   }
+// }
+
+const Search = ({ value, onChange, children }) => {
+  return (
+    <form>
+      {children} <input
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </form>
+  )
+}
+
+const largeColumn = {
+  width: '40%',
+};
+
+const midColumn = {
+  width: '30%',
+};
+
+const smallColumn = {
+  width: '10%',
+};
+
+const Table = ({ list, pattern, onDismiss }) =>
+  <div className="table">
+    {list.filter(isSearch(pattern)).map(item =>
+      <div key={item.objectID} className="table-row">
+        <span style={largeColumn}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: '30%' }}>
+          {item.author}
+        </span>
+        <span style={smallColumn}>
+          {item.num_comments}
+        </span>
+        <span style={smallColumn}>
+          {item.points}
+        </span>
+        <span style={{ width: '10%' }}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    )}
+  </div>
+
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className = '',
+      children,
+    } = this.props;
+
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
     );
   }
 }
